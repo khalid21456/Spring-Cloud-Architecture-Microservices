dc09b0148caf7ba0cd6260095bbe871d13c22aaa
khalid21456 <khalidedaoudi8@gmail.com>
2025-11-16 01:17:18 +0100

    billing service/openfeign

--- billing-service/src/main/java/com/example/billing_service/feign/InventoryServiceRestClient.java
+++ billing-service/src/main/java/com/example/billing_service/feign/InventoryServiceRestClient.java
@@ -2,6 +2,7 @@ package com.example.billing_service.feign;
 
 
 import com.example.billing_service.models.Product;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,5 +13,10 @@ import java.util.UUID;
 @FeignClient(name = "inventory-service")
 public interface InventoryServiceRestClient {
     @GetMapping("/products/{id}")
+    @CircuitBreaker(name = "inventory-service", fallbackMethod = "getDefaultProduct")
     Product getProduct(@PathVariable UUID id);
+
+    default Product getDefaultProduct(UUID id, Exception ex ) {
+        return Product.builder().uuid(id).build();
+    }
 }
